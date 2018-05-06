e7752920b32d5dd218e486808a6751960877ede3
孙天阳 <jsjtsty@163.com>
2018-05-06 14:53:57 +0800

    Configure OpenSSL.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <openssl/crypto.h>
+#include <openssl/md5.h>
 
 extern "C" JNIEXPORT jstring
 
@@ -7,6 +9,6 @@ JNICALL
 Java_tk_stystudio_fileencryptor_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = OpenSSL_version(OPENSSL_VERSION);
     return env->NewStringUTF(hello.c_str());
 }
